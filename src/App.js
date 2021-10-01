88961500f8d3aa5e3ecd19417ad94971bb0937cf
imros10 <imros.arefen@gmail.com>
2021-10-01 20:23:31 +0600

    'first'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Header from './component/Header/Header';
-import Main from './component/Main/Main';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Main></Main>
+      <Shop></Shop>
     </div>
   );
 }
