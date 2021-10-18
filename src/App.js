08a6fa038554e0e8c0492fc5976c7f4724ed2fab
imros10 <imros.arefen@gmail.com>
2021-10-18 10:33:18 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
 import './App.css';
-import Header from './component/Header/Header';
-import Footer from './component/Footer/Footer';
-import Home from './component/Home/Home';
-import AllServices from './component/AllServices/AllServices';
-import { BrowserRouter, Switch,Route } from 'react-router-dom';
-import About from './component/About/About';
-import NotFound from './component/NotFound/NotFound';
-import Contact from './component/Contact/Contact';
-
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Header></Header>
-      <Switch>
-        <Route exact path='/'>
-        <Home></Home>
-        </Route>
-        <Route path='/home'>
-        <Home></Home>
-        </Route>
-        <Route path='/services'>
-        <AllServices></AllServices>
-        </Route>
-        <Route path='/about'>
-        <About></About>
-        </Route>
-        <Route path='/contact'>
-        <Contact></Contact>
-        </Route>
-        <Route path='*'>
-        <NotFound></NotFound>
-        </Route>
-      </Switch>
-      <Footer></Footer>
-      </BrowserRouter>
+     
     </div>
   );
 }
-
 export default App;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfzu-541TNEu5M1nB_miPISB6_Z1YnesU",
+  authDomain: "high-check-imros.firebaseapp.com",
+  projectId: "high-check-imros",
+  storageBucket: "high-check-imros.appspot.com",
+  messagingSenderId: "380667188708",
+  appId: "1:380667188708:web:a3af40865f83dc067c23f1"
+};
+export default firebaseConfig;
