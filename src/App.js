2deab83ad2c71a7ef25acd5c4c428e00da21871f
imros10 <imros.arefen@gmail.com>
2021-10-29 23:09:31 +0600

    add items

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
-import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import About from './component/About/About';
-import Contact from './component/Contact/Contact';
-import Footer from './component/Footer/Footer';
-import Header from './component/Header/Header';
-import Home from './component/Home/Home';
-import Login from './component/Login/Login';
-import NotFound from './component/NotFound/NotFound';
-import PrivateRoute from './component/PrivateRoute/PrivateRoute';
-import ServiceInfo from './component/ServiceInfo/ServiceInfo';
-import ProvideAuth from './ProvideAuth/ProvideAuth';
 
 function App() {
   return (
     <div className="App">
-     <ProvideAuth>
-     <BrowserRouter>
-     <Header></Header>
-     <Switch>
-    <Route exact path="/">
-    <Home></Home>
-    </Route>
-    <Route path="/home">
-    <Home></Home>
-    </Route>
-    <Route path="/login">
-    <Login></Login>
-    </Route>
-    
-    <PrivateRoute path="/service-info/:serviceid">
-      <ServiceInfo></ServiceInfo>
-    </PrivateRoute>
-    <PrivateRoute path="/about">
-    <About></About>
-    </PrivateRoute>
-    <PrivateRoute path="/contact">
-    <Contact></Contact>
-    </PrivateRoute>
-    <Route path="*">
-      <NotFound></NotFound>
-    </Route>
-     </Switch>
-     <Footer></Footer>
-     </BrowserRouter>
-     </ProvideAuth>
+      
     </div>
   );
 }
+
 export default App;
--- src/Firebase/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAEqgPgUokI41fmmYMAavjFgRyrmne968E",
-  authDomain: "counseling-b75a4.firebaseapp.com",
-  projectId: "counseling-b75a4",
-  storageBucket: "counseling-b75a4.appspot.com",
-  messagingSenderId: "392981660081",
-  appId: "1:392981660081:web:a88fa0e7a603452af8bd90"
-};
-export default firebaseConfig;
