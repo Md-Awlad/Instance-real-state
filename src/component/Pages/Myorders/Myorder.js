6d7313baf262f152f23dae55c9ae0c093943c7c4
imros10 <imros.arefen@gmail.com>
2021-11-17 14:11:00 +0600

    100% complete

--- src/component/Pages/Myorders/Myorder.js
+++ src/component/Pages/Myorders/Myorder.js
@@ -16,11 +16,8 @@ const Myorder = () => {
         <div className='section-container'>
             <div className="container">
                 <h1 className='section-title'>My Orders</h1>
-                {!myorderInfo.length && <div class="d-flex justify-content-center">
-                    <div class="spinner-border" role="status">
-                        <span class="visually-hidden">Loading...</span>
-                    </div>
-                    </div>}
+                {!myorderInfo.length && <p className='text-center text-danger'><b >No Data found</b></p>
+                   }
                 <div className="row row-cols-1 row-cols-md-3 row-cols-sm-1 g-5">
                     {
                         myorderInfo?.map(item=><OrderCard key={item._id} values={item}></OrderCard>)
