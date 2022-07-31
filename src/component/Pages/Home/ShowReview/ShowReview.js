be0f39b9d2604a3b766bdb25f9061f7d79aab31b
Md-Awlad <mdawladbdinfo@gmail.com>
2022-08-01 00:54:46 +0600

    Review add

--- src/component/Pages/Home/ShowReview/ShowReview.js
+++ src/component/Pages/Home/ShowReview/ShowReview.js
@@ -1,46 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import SingleReviw from './SingleReviw/SingleReviw';
+import SingleReviw from "./SingleReviw/SingleReviw";
 const ShowReview = () => {
-    const [reviws,setReviws] = useState([]);
-    useEffect(()=>{
-        fetch('https://stormy-citadel-14201.herokuapp.com/review')
-        .then(res=>res.json())
-        .then(data=>setReviws(data))
-    },[])
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 3
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-    return (
-        <div className='section-container show-review-section'>
-            <div className="container">
-                <h1 className='section-title'>Reviews</h1>
-                <Carousel responsive={responsive}>
-                {
-                    reviws.map(item=><SingleReviw key={item._id} values={item}></SingleReviw>)
-                }
-                </Carousel>
-            </div>
-            
-        </div>
-    );
+  const [reviws, setReviws] = useState([]);
+  useEffect(() => {
+    fetch("https://stormy-citadel-14201.herokuapp.com/review")
+      .then((res) => res.json())
+      .then((data) => setReviws(data));
+  }, []);
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 3,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <div className="section-container show-review-section">
+      <div className="container">
+        <h1 className="section-title">Review</h1>
+        <Carousel responsive={responsive}>
+          {reviws.map((item) => (
+            <SingleReviw key={item._id} values={item}></SingleReviw>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default ShowReview;
